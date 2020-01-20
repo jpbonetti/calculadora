7940209c811005c41fac4a4b1aa3fec8940356ae
Joao Paulo Bonetti Rodrigues <joaobr@lnxcit024207.cit>
2020-01-20 16:14:52 -0300

    Added html components

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -9,9 +9,39 @@ import { CalculadoraService } from '../services';
 })
 export class CalculadoraComponent implements OnInit {
 
+  private number1: string;
+  private number2: string;
+  private result: number;
+  private operation: number;
+
   constructor(private calculadoraService: CalculadoraService) { }
 
   ngOnInit() {
+    this.clean();
   }
 
+  clean(): void {
+    this.number1 = '0';
+    this.number2 = null;
+    this.result = null;
+    this.operation = null;
+  }
+
+  addNumber(num: string): void {
+    if (this.number1 !== null) {
+      this.number2 = num;
+    } else {
+      this.number1 = num;
+    }
+  }
+
+  get display(): string {
+    if (this.result !== null) {
+        return this.result.toString();
+    }
+    if (this.number2 !== null) {
+      return this.number2;
+    }
+    return this.number1;
+  }
 }
